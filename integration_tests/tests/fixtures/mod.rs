c56f742df1303234351134d26f5a63a8979f3133
aoikurokawa <ht.so.ik0102@gmail.com>
2025-01-12 13:29:35 +0900

    refact: remove comments

--- integration_tests/tests/fixtures/mod.rs
+++ integration_tests/tests/fixtures/mod.rs
@@ -1,6 +1,5 @@
-use solana_program::{instruction::InstructionError, program_error::ProgramError};
+use solana_program::program_error::ProgramError;
 use solana_program_test::BanksClientError;
-use solana_sdk::transaction::TransactionError;
 use thiserror::Error;
 
 pub mod fixture;
@@ -14,27 +13,4 @@ pub enum TestError {
     BanksClientError(#[from] BanksClientError),
     #[error(transparent)]
     ProgramError(#[from] ProgramError),
-}
-
-impl TestError {
-    pub fn to_transaction_error(&self) -> Option<TransactionError> {
-        match self {
-            TestError::BanksClientError(e) => match e {
-                BanksClientError::TransactionError(e) => Some(e.clone()),
-                BanksClientError::SimulationError { err, .. } => Some(err.clone()),
-                _ => None,
-            },
-            TestError::ProgramError(_) => None,
-        }
-    }
-}
-
-#[inline(always)]
-#[track_caller]
-pub fn assert_ix_error<T>(test_error: Result<T, TestError>, ix_error: InstructionError) {
-    assert!(test_error.is_err());
-    assert_eq!(
-        test_error.err().unwrap().to_transaction_error().unwrap(),
-        TransactionError::InstructionError(0, ix_error)
-    );
 }
